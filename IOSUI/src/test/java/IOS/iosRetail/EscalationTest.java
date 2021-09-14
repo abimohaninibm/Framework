9f427a7eece4d6353a16fd6aac74baab3a5596c3
060328744 <060328744@ABIMOHAN.in.ibm.com>
2021-09-14 23:39:56 +0530

    AssertFail and Modified the testng.xml

--- IOSUI/src/test/java/IOS/iosRetail/EscalationTest.java
+++ IOSUI/src/test/java/IOS/iosRetail/EscalationTest.java
@@ -1,5 +1,6 @@
 package IOS.iosRetail;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class EscalationTest {
@@ -7,5 +8,6 @@ public class EscalationTest {
 	@Test
 	public void escalation() {		
 		System.out.println("escalation TestCase");
+		Assert.assertFalse(true);
 	}
 }

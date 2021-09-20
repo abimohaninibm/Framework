36e21d1b543dcaef3b4b932d79cd78f9eb9781a6
060328744 <060328744@ABIMOHAN.mshome.net>
2021-09-21 00:15:26 +0530

    Added a selenium test case in the EscalationTest.java

--- IOSUI/src/test/java/IOS/iosRetail/EscalationTest.java
+++ IOSUI/src/test/java/IOS/iosRetail/EscalationTest.java
@@ -1,5 +1,7 @@
 package IOS.iosRetail;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -10,5 +12,14 @@ public class EscalationTest {
 		System.out.println("escalation TestCase");
 		//Assert.assertFalse(true);
 		
+		System.setProperty("wedriver.chrome.driver", "chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.amazon.in/");
+		System.out.println(driver.getTitle());
+		
+		Assert.assertEquals(driver.getTitle(), "Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in");
+		
+		driver.close();
 	}
 }
